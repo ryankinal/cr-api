7e14237c13b7a3267106ac231df7a557c58762aa
Ryan Kinal <ryan.kinal@gmail.com>
2014-02-28 07:53:58 -0500

    Implemented basic roll object initialization

--- models/roll.js
+++ models/roll.js
@@ -1,3 +1,16 @@
+var validTypes = [2, 4, 6, 8, 10, 12, 20, 30, 100];
+
 module.exports = {
-	init: function() {}
+	init: function(type, value, certified) {
+		if (validTypes.indexOf(type) === -1 && !(/^10{2,}$/.test(type)))
+		{
+			throw('Invalid die type');
+		}
+
+		this.type = type;
+		this.value = value;
+		this.used = false;
+		this.certified = certified;
+		this.dateAdded = Date.now();
+	}
 };
