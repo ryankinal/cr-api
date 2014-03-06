a6fcfc0db422220f7233307807b31620150432a6
Ryan Kinal <ryan.kinal@gmail.com>
2014-03-05 20:12:24 -0500

    Added dependencies for q and firebase libraries.

--- models/roll-repository.js
+++ models/roll-repository.js
@@ -1,1 +1,25 @@
-module.exports = {};
+var q = require('q'),
+	firebase = require('firebase');
+
+module.exports = {
+	get: function() {
+		var deferred = q.defer();
+		return deferred.promise;
+	},
+	find: function() {
+		var deferred = q.defer();
+		return deferred.promise;
+	},
+	insert: function() {
+		var deferred = q.defer();
+		return deferred.promise;
+	},
+	update: function() {
+		var deferred = q.defer();
+		return deferred.promise;
+	},
+	remove: function() {
+		var deferred = q.defer();
+		return deferred.promise;
+	}
+};
