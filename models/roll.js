358bc5910902bf5879bfceb3c36be17d32eefc2e
Ryan Kinal <ryan.kinal@gmail.com>
2014-03-05 20:11:56 -0500

    Fixed variable declaration in Roll::init

--- models/roll.js
+++ models/roll.js
@@ -2,15 +2,36 @@ var validTypes = [2, 4, 6, 8, 10, 12, 20, 30, 100];
 
 module.exports = {
 	init: function(type, value, certified) {
-		if (validTypes.indexOf(type) === -1 && !(/^10{2,}$/.test(type)))
+		var config;
+
+		if (typeof type === 'Object')
+		{
+			config = type;
+		}
+		else
+		{
+			config = {
+				type: type,
+				certified: certified,
+				value: value
+			};
+		}
+
+		if (validTypes.indexOf(config.type) === -1 && !(/^10{2,}$/.test(config.type)))
 		{
 			throw('Invalid die type');
 		}
 
-		this.type = type;
-		this.value = value;
-		this.used = false;
-		this.certified = certified;
-		this.dateAdded = Date.now();
+		if (config.value > config.type)
+		{
+			throw('The value of a roll may not be greater than its type');
+		}
+
+		this.type = config.type;
+		this.value = config.value;
+		this.certified = config.certified;
+		this.dateAdded = config.dateAdded || undefined;
+		this.used = config.used || false;
+		this.dateUsed = config.dateUsed || undefined;
 	}
 };
