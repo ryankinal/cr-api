90a3b606862f2b23fe80207205f63f9b46d06c2b
Ryan Kinal <ryan.kinal@gmail.com>
2014-05-06 22:51:58 -0400

    Added actual endpoints for getting roll data

---
+++ app.js
@@ -0,0 +1,85 @@
+var express = require('express'),
+	repo = require('./models/roll-repository'),
+	config = require('./config-test'),
+	app = express();
+
+app.configure(function() {
+	app.use(express.bodyParser());
+});
+
+app.post('/roll', function(req, res) {
+	if (repo.validate(req.body)) {
+		repo.create(roll).then(function(roll) {
+			res.json({
+				meta: {
+					success: true
+				},
+				roll: roll
+			});
+		});
+	} else {
+		res.statusCode = 400;
+		res.json({
+			meta: {
+				success: false
+			},
+			error: 'Invalid roll data'
+		});
+	}
+});
+
+app.get('/roll', function(req, res) {
+	if (req.query.type) {
+		repo.getRandom(parseInt(req.query.type, 10)).then(function(roll) {
+			if (roll) {
+				res.json(roll);
+			} else {
+				res.statusCode = 404;
+				res.json({
+					meta: {
+						success: false
+					},
+					error: 'No rolls of type ' + req.query.type + ' found'
+				});
+			}
+		}, function(err) {
+			res.statusCode = 404;
+			res.json({
+				meta: {
+					success: false,
+					err: err
+				},
+				error: 'No rolls of type ' + req.query.type + ' found'
+			});
+		});
+	} else {
+		res.statusCode = 400;
+		res.json({
+			meta: {
+				success: false
+			},
+			error: 'A roll type (die type) must be provided in the query string'
+		})
+	}
+});
+
+app.get('/roll/:id', function(req, res) {
+	repo.get(req.params.id).then(function(roll) {
+		res.json({
+			meta: {
+				success: true
+			},
+			roll: roll
+		})
+	}, function() {
+		res.statusCode = 404;
+		res.json({
+			meta: {
+				success: false
+			},
+			error: 'Roll ID ' + req.params.id + ' not found'
+		});
+	});
+});
+
+app.listen(process.env.PORT || 4444);
